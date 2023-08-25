12690bda256611116dc2b6d828c083ab42f0f9b4
Joko Endriyanto <46419237+jokoendriyanto@users.noreply.github.com>
2023-08-25 23:43:08 +0700

    Import Stisla Theme

--- resources/views/pages/blank-page.blade.php
+++ resources/views/pages/blank-page.blade.php
@@ -9,7 +9,7 @@
 @section('main')<div class="main-content">
         <section class="section">
             <div class="section-header">
-                <h1>Blank Page</h1>
+                <h1>Blank Page 123</h1>
             </div>
 
             <div class="section-body">
